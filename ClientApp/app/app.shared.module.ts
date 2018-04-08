a8bf999eaf715f8d797de3827cd609bb11acb186
Nikola Bukovac <nikola.bukovac@outlook.com>
2018-04-08 14:13:15 +0200

    Removed unused components

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -7,16 +7,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent,
         ChatComponent
     ],
@@ -27,8 +23,6 @@ import { ChatComponent } from './components/chat/chat.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'chat-app', component: ChatComponent},
             { path: '**', redirectTo: 'home' }
         ])
